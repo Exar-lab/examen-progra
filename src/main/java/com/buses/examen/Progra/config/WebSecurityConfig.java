4f515c0cd57fa22d82ad655985b024fb00fbc904
Exar-lab <exarviquezmasis@gmail.com>
2026-05-09 15:24:53 -0600

    feat(security): expose authentication beans

--- src/main/java/com/buses/examen/Progra/config/WebSecurityConfig.java
+++ src/main/java/com/buses/examen/Progra/config/WebSecurityConfig.java
@@ -3,10 +3,14 @@ package com.buses.examen.Progra.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -59,5 +63,13 @@ public class WebSecurityConfig {
                         .anyRequest().authenticated());
 
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
